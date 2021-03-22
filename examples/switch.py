159ea2ea5738c67af7456305668f4030be71c11a
4o <githubmail.4o@ya.ru>
2021-03-22 03:35:53 +0000

    add switch example

---
+++ examples/switch.py
@@ -0,0 +1,14 @@
+from nyanMigen import nyanify
+
+
+@nyanify()
+def switch():
+    a = Signal(2)
+    b = Signal(2)
+
+    with switch(a):
+        b = 3
+        with case(0):
+            b = 1
+        with case(1):
+            b = 0
