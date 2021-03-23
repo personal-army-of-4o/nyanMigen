47517ac6287d659f852099be18d97f9a8ed0c73c
4o <githubmail.4o@ya.ru>
2021-03-23 05:35:27 +0000

    run each test in separate process

--- run_test.bash
+++ run_test.bash
@@ -1,26 +1,20 @@
 #!/bin/bash
 
 
+onetest=`pwd`/onetest.bash
 exs="examples/"
 cd $exs
 src=`ls | grep .py$`
 
 echo $src
 
+n=0
 for i in $src; do
-    n="${i%.*}"
-    o=$n.nmigen
-    v=$n.v
-
-    rm $o -f
-
-    e="python3 $i generate $v"
-    echo $e "> $o"
-    $e > $o
-    echo "if __name__ == '__main__':" >> $o
-    echo "    $n.main()" >> $o
+    bash $onetest $i &
+    pids[${n}]=$!
+    n=$n+1
 done
-exit 0
 
-python3 test.py 1>doc/example.md
-cp ram.stat doc/example.stat
+for pid in ${pids[*]}; do
+    wait $pid
+done
