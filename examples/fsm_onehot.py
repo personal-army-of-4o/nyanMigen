54df0d521d847d5f89dbbed239e5058fb0a08ef6
4o <githubmail.4o@ya.ru>
2021-03-26 08:59:15 +0000

    fix fsm state list extraction

--- examples/fsm_onehot.py
+++ examples/fsm_onehot.py
@@ -24,16 +24,16 @@ def fsm_onehot():
 
     with switch(state):
         with case('one'):
+            state = 'two'
+        with case('two'):
+            state = 'three'
+        with case('three'):
+            state = 'four'
+
+    with switch(state):
+        with case('one'):
             o = 0
         with case('two'):
             o = 0
         with case('three'):
             o = 1
-
-    with switch(state):
-        with case('one'):
-            sync.state = 'two'
-        with case('two'):
-            sync.state = 'three'
-        with case('three'):
-            sync.state = 'four'
--- examples/indexing.py
+++ examples/indexing.py
@@ -7,8 +7,8 @@ class indexing:
         n = 3
         inp = Signal(n**n)
         outp = Signal(n**n)
-        ari = Array(Array(Signal() for _ in range(n)) for _ in range(n))
-        aro = Array(Array(Signal() for _ in range(n)) for _ in range(n))
+        ari = Array(Array(Array(Signal() for _ in range(n)) for _ in range(n)) for _ in range(n))
+        aro = Array(Array(Array(Signal() for _ in range(n)) for _ in range(n)) for _ in range(n))
 
         for i in range(n):
             for j in range(n):
